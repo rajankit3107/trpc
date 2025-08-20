158098065d10f8df694a8aff8f8c80da527ac79e
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-20 16:23:02 +0530

    understanding mutation

---
+++ client/index.ts
@@ -0,0 +1,23 @@
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "../server";
+//     👆 **type-only** import
+// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
+// what procedures are available on the server and their input/output types.
+const trpc = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3001",
+    }),
+  ],
+});
+
+async function main() {
+  let response = await trpc.signUp.mutate({
+    username: "ankit",
+    password: "123456",
+  });
+
+  console.log(response);
+}
+
+main();
--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,48 @@
-import { publicProcedure, router } from './trpc';
-import { z } from 'zod'
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
 
 const todoInputType = z.object({
-    title : z.string(),
-    description : z.string()
-})
- 
+  title: z.string(),
+  description: z.string(),
+});
 
 const appRouter = router({
-  createTodo : publicProcedure
-  .input(todoInputType)
-  .mutation(async (opts) => {
-    const title = opts.input.title;
-    const description = opts.input.description;
+  createTodo: publicProcedure
+    .input(todoInputType)
+    .mutation(async (opts: { input: z.infer<typeof todoInputType> }) => {
+      const title = opts.input.title;
+      const description = opts.input.description;
 
+      return {
+        id: "1",
+      };
+    }),
+  signUp: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        password: z.string(),
+      })
+    )
+    .mutation(async (opts) => {
+      let username = opts.input.username;
+      let password = opts.input.password;
 
-    return {
-        id : "1",
-    }
-  })
+      let token = "`213131314e3";
+
+      return {
+        token,
+      };
+    }),
 });
 
 const server = createHTTPServer({
-    router: appRouter,
-  });
-   
-  server.listen(3000);
- 
+  router: appRouter,
+});
+
+server.listen(3001);
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
